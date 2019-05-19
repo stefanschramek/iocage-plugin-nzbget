ac4e112d3416e3f2c02497440908aaa9952b23c3
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-05-19 20:32:39 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -12,4 +12,7 @@ pw user add nzbget -c nzbget -u 504 -d /nonexistent -s /usr/bin/nologin
 
 chown -R nzbget:nzbget /usr/local/nzbget
 
-/usr/local/etc/rc.d
+fetch https://github.com/stefanschramek/iocage-plugin-nzbget/blob/master/nzbget -o /etc/rc.d/
+chmod u+x /etc/rc.d/nzbget
+sysrc "nzbget_enable=YES"
+service nzbget start
