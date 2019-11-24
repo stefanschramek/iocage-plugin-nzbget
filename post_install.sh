25b44d37449e92de243b9e03c288938ef4eada8d
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-11-24 22:05:36 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -9,9 +9,10 @@ sh /usr/local/nzbget-latest-bin-freebsd.run --destdir /usr/local/nzbget
 rm /usr/local/nzbget-latest-bin-freebsd.run
 
 pw user add nzbget -c nzbget -u 504 -d /nonexistent -s /usr/bin/nologin
-
 chown -R nzbget:nzbget /usr/local/nzbget
 
 chmod u+x /etc/rc.d/nzbget
-sysrc "nzbget_enable=YES"
-service nzbget start
+sysrc -f /etc/rc.conf nzbget_enable="YES"
+service nzbget start 2>/dev/null
+
+echo "NZBGet successfully installed" > /root/PLUGIN_INFO
