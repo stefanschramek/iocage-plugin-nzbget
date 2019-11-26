8ed3b5676c01286127aae111daabd83de887d2ff
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-11-26 18:29:45 +0100

    Add group for access to mounted storage

--- post_install.sh
+++ post_install.sh
@@ -11,6 +11,10 @@ rm /usr/local/nzbget-latest-bin-freebsd.run
 pw user add nzbget -c nzbget -u 504 -d /nonexistent -s /usr/bin/nologin
 chown -R nzbget:nzbget /usr/local/nzbget
 
+#Add group for access to mounted storage
+pw groupadd -n mnt -g 1002
+pw groupmod 1002 -m 504
+
 chmod u+x /etc/rc.d/nzbget
 sysrc -f /etc/rc.conf nzbget_enable="YES"
 service nzbget start 2>/dev/null
